0f1fea1cadda320c14d9eb0309359777758aaf04
Eduardo <144747945+eddchinea@users.noreply.github.com>
2024-06-07 14:25:35 -0400

    Add loop to build board and add a Mark() in each square

--- code.js
+++ code.js
@@ -2,4 +2,11 @@ function gameBoard () {
     const rows = 3;
     const columns = 3;
     const board = [];
+
+    for (let i = 0; i < rows; i++) {
+        board[i] = [];
+        for (let j = 0; i < columns; j++) {
+            board[i].push(Mark())
+        }
+    }
 }
